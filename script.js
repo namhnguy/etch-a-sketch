c73c8dff37075941faabf9663e060596371cdcc8
Nam Nguyen <namhnguy@gmail.com>
2022-01-14 01:02:22 -0600

    add in new functions

--- script.js
+++ script.js
@@ -2,28 +2,30 @@ const grid = document.querySelector(".container");
 let gridSize = 16;
 let mouseIsDown = false;
 
-for (i = 0; i < 256; i++) {
-    const gridItem = document.createElement("div");
-    gridItem.classList.add('grid-item');
-    gridItem.addEventListener('mousedown', function (e) {
-        e.target.style.backgroundColor = 'black';
-        mouseIsDown = true;
-    })
-    gridItem.addEventListener('mouseover', function (e) {
-        if (mouseIsDown) {
+function createDefaultGrid() {
+    for (i = 0; i < 256; i++) {
+        const gridItem = document.createElement("div");
+        gridItem.classList.add('grid-item');
+        gridItem.addEventListener('mousedown', function (e) {
             e.target.style.backgroundColor = 'black';
-        }
-    })
-    gridItem.addEventListener('mouseup', function () {
-        mouseIsDown = false;
-    })
-    gridItem.addEventListener('dragstart', (e) => {
-        e.preventDefault();
-    })
-    gridItem.addEventListener('drop', (e) => {
-        e.preventDefault();
-    })
-    grid.appendChild(gridItem);
+            mouseIsDown = true;
+        })
+        gridItem.addEventListener('mouseover', function (e) {
+            if (mouseIsDown) {
+                e.target.style.backgroundColor = 'black';
+            }
+        })
+        gridItem.addEventListener('mouseup', function () {
+            mouseIsDown = false;
+        })
+        gridItem.addEventListener('dragstart', (e) => {
+            e.preventDefault();
+        })
+        gridItem.addEventListener('drop', (e) => {
+            e.preventDefault();
+        })
+        grid.appendChild(gridItem);
+    }
 }
 
 const btn = document.querySelector('#btn-dim');
@@ -69,3 +71,52 @@ reset.addEventListener('click', function () {
         grid.children[i].style.backgroundColor = "white";
     }
 })
+
+const colorChooser = document.querySelector('#color-input');
+colorChooser.addEventListener('input', function() {
+    let colorPick = document.getElementById('color-input').value;
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        grid.children[i].addEventListener('mousedown', function(e) {
+            e.target.style.backgroundColor = colorPick;
+            mouseIsDown = true;
+        })
+        grid.children[i].addEventListener('mouseover', function(e) {
+            if (mouseIsDown) {
+                e.target.style.backgroundColor = colorPick;
+            }
+        })
+    }
+})
+
+const eraser = document.querySelector('#btn-eraser');
+eraser.addEventListener('click', function() {
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        grid.children[i].addEventListener('mousedown', function(e) {
+            e.target.style.backgroundColor = 'white';
+            mouseIsDown = true;
+        })
+        grid.children[i].addEventListener('mouseover', function(e) {
+            if (mouseIsDown) {
+                e.target.style.backgroundColor = 'white';
+            }
+        })
+    }
+})
+
+const draw = document.querySelector('#btn-draw');
+draw.addEventListener('click', function() {
+    let colorPick = document.getElementById('color-input').value;
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        grid.children[i].addEventListener('mousedown', function(e) {
+            e.target.style.backgroundColor = colorPick;
+            mouseIsDown = true;
+        })
+        grid.children[i].addEventListener('mouseover', function(e) {
+            if (mouseIsDown) {
+                e.target.style.backgroundColor = colorPick;
+            }
+        })
+    }
+})
+
+createDefaultGrid();
